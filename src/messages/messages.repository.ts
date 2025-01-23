1147c70658089e8c733594f04b29378d19ef1db5
pradeeprajkk <pradeep.raj@accionlabs.com>
2025-01-23 13:59:51 +0530

    depedency injection

--- src/messages/messages.repository.ts
+++ src/messages/messages.repository.ts
@@ -1,5 +1,8 @@
+import { Injectable } from "@nestjs/common";
+
 import { readFile, writeFile } from "fs/promises";
 
+@Injectable()
 export class MessagesRepository {
   async findOne(id: string) {
     const contents = await readFile('messages.json', 'utf-8');
