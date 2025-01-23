75e47d045aa4243ee04db43515fb3e0430d355e4
pradeeprajkk <pradeep.raj@accionlabs.com>
2025-01-23 11:42:35 +0530

    Addd Repository and Service

---
+++ src/messages/messages.repository.ts
@@ -0,0 +1,29 @@
+import { readFile, writeFile } from "fs/promises";
+
+export class MessagesRepository {
+  async findOne(id: string) {
+    const contents = await readFile('messages.json', 'utf-8');
+    const messages = JSON.parse(contents);
+
+    return messages[id];
+  }
+
+  async create(content: string) {
+    const contents = await readFile('messages.json', 'utf-8');
+    const messages = JSON.parse(contents);
+
+    const id = Math.floor(Math.random() * 999);
+
+    messages[id] = { id, content };
+
+    await writeFile('messages.json', JSON.stringify(messages));
+    return true;
+  }
+
+  async findAll() {
+    const contents = await readFile('messages.json', 'utf-8');
+    const messages = JSON.parse(contents);
+
+    return messages;
+  }
+}
